1ca3b0b8eb926f801e949b36d69d70fe71833ef3
arod12345 <sisayabel681@gmail.com>
2024-10-16 21:05:35 +0300

    added vercel config file

--- Admin-Dashboard/src/components/LoginCard.jsx
+++ Admin-Dashboard/src/components/LoginCard.jsx
@@ -1,49 +1,46 @@
 import React, { useState, useEffect } from "react";
-import {useNavigate,Link} from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useLoginMutation } from "../slices/adminApiSlice";
 import { setCredentials } from "../slices/authSlice";
-import {toast} from 'react-toastify'
-import {FaEye, FaEyeSlash, FaSignInAlt} from 'react-icons/fa'
-import Spinner from './Spinner'
-import Button from './shared/Button'
+import { toast } from "react-toastify";
+import { FaEye, FaEyeSlash, FaSignInAlt } from "react-icons/fa";
+import Spinner from "./Spinner";
+import Button from "./shared/Button";
 
 const LoginCard = () => {
-  const [idNumber,setIdNumber] = useState("");
+  const [idNumber, setIdNumber] = useState("");
   const [password, setPassword] = useState("");
-  const [revealPassword,setRevealPassword]=useState(false)
+  const [revealPassword, setRevealPassword] = useState(false);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const [login, { isLoading }] = useLoginMutation();
 
-  const { adminInfo } = useSelector(state => state.auth);
+  const { adminInfo } = useSelector((state) => state.auth);
 
-  useEffect(
-    () => {
-      if (adminInfo) {
-        navigate("/dashboard");
-      }
-    },
-    [navigate, adminInfo]
-  );
+  useEffect(() => {
+    if (adminInfo) {
+      navigate("/dashboard");
+    }
+  }, [navigate, adminInfo]);
 
-  const submitHandler = async e => {
+  const submitHandler = async (e) => {
     e.preventDefault();
     try {
       const res = await login({ idNumber, password }).unwrap();
-      dispatch(setCredentials({...res}));
-      toast.success("User Logged in Succesfuly")
-      navigate('/')
+      dispatch(setCredentials({ ...res }));
+      toast.success("User Logged in Succesfuly");
+      navigate("/");
     } catch (err) {
-        toast.error(err?.data?.message || err.error)
+      toast.error(err?.data?.message || err.error);
     }
   };
 
-  const passwordReval=()=>{
-    setRevealPassword(prev=>!prev)
-  }
+  const passwordReval = () => {
+    setRevealPassword((prev) => !prev);
+  };
 
   return (
     <div className="flex w-full flex-col items-center">
@@ -53,6 +50,12 @@ const LoginCard = () => {
         onSubmit={submitHandler}
       >
         <label htmlFor="#idnumber">ID Number</label>
+        <label className="text-yellow-600 font-bold">
+          {" "}
+          ***Use Default ID Number <span className="text-red-600">
+            1234
+          </span>{" "}
+        </label>
         <div className="inputBOx"></div>
         <input
           id="idnumber"
@@ -60,34 +63,53 @@ const LoginCard = () => {
           placeholder="Enter  ID Number"
           className="p-3  my-2 bg-transparent rounded-lg border-[.5px] border-[#ffffff50] focus:border-accentColor outline-none"
           value={idNumber}
-          onChange={e => setIdNumber(e.target.value)}
+          onChange={(e) => setIdNumber(e.target.value)}
         />
         <label htmlFor="#password">Password</label>
+        <label className="text-yellow-600 font-bold">
+          {" "}
+          ***Use Default Password <span className="text-red-600">
+            123456
+          </span>{" "}
+        </label>
 
         <div className="flex  items-center justify-between my-2 mb-6 rounded-lg border-[.5px] border-[#ffffff50] focus:border-accentColor hover:border-accentColor">
-        <input
-          id="password"
-          type={revealPassword?"text":"password"}
-          className="p-3 bg-transparent outline-none w-full"
-          placeholder="Enter password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-     
-        {revealPassword?
-        <div onClick={passwordReval} className="bg-[#6495ed] py-[.35em] rounded-md text-white cursor-pointer px-4  mr-2">hide</div>
-        :<div onClick={passwordReval} className="bg-[#6495ed] py-[.35em] rounded-md text-white cursor-pointer px-4  mr-2">show</div>
-          }
+          <input
+            id="password"
+            type={revealPassword ? "text" : "password"}
+            className="p-3 bg-transparent outline-none w-full"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {revealPassword ? (
+            <div
+              onClick={passwordReval}
+              className="bg-[#6495ed] py-[.35em] rounded-md text-white cursor-pointer px-4  mr-2"
+            >
+              hide
+            </div>
+          ) : (
+            <div
+              onClick={passwordReval}
+              className="bg-[#6495ed] py-[.35em] rounded-md text-white cursor-pointer px-4  mr-2"
+            >
+              show
+            </div>
+          )}
         </div>
 
         <Button
-           buttonContent="Sign In"
-           width="100%"
-           icon={isLoading ? <Spinner/>:<FaSignInAlt className="mr-3"/>}
-           py='.5em'
+          buttonContent="Sign In"
+          width="100%"
+          icon={isLoading ? <Spinner /> : <FaSignInAlt className="mr-3" />}
+          py=".5em"
         />
         <Link to="/register" className="mt-3">
-          <p className="w-full px-4 py-3 border text-white border-indigo-500 rounded-md">Don't Have an Account yet Register?</p>   
+          <p className="w-full px-4 py-3 border text-white border-indigo-500 rounded-md">
+            Don't Have an Account yet Register?
+          </p>
         </Link>
       </form>
     </div>
